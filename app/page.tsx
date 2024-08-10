aed46d3559ae9a08d87d52976dc35e413dc02754
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-08-09 23:21:59 -0700

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Header from '@/components/Header';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="bg-text pt-4">
+    <main className="bg-text">
       <Header />
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,20 +4,46 @@ import { Menu } from 'lucide-react';
 import { FaWallet } from 'react-icons/fa6';
 import { IoChatbox } from 'react-icons/io5';
 import { IoIosNotifications } from 'react-icons/io';
+import profile from '../public/profile.jpg';
+import Image from 'next/image';
+import { Input } from './ui/input';
 
 const Header = () => {
   return (
-    <nav className="flex items-center justify-between px-4 h-16 bg-text">
-      <Button variant="secondary" size={'icon'} className="rounded-sm size-12">
-        <Menu className="text-accent size-8" />
-      </Button>
+    <nav className="flex items-center justify-between px-4 md:px-10 pt-4 md:pt-12 h-16 bg-text">
+      <div className="flex items-center gap-4">
+        <Button
+          variant="secondary"
+          size={'icon'}
+          className="rounded-sm size-12"
+        >
+          <Menu className="text-accent size-8" />
+        </Button>
+        <div className="hidden md:flex items-center gap-2">
+          <Input type="text" placeholder="Search" className="min-w-[400px]" />
+        </div>
+      </div>
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-4">
-          <FaWallet className="text-accent size-6" />
-          <IoChatbox className="text-accent size-6" />
-          <IoIosNotifications className="text-accent size-8" />
+          <Button variant={'secondary'} size={'icon'} className="rounded-full">
+            <FaWallet className="text-accentV size-6" />
+          </Button>
+          <Button variant={'secondary'} size={'icon'} className="rounded-full">
+            <IoIosNotifications className="text-accentV size-8" />
+          </Button>
+          <Button variant={'secondary'} size={'icon'} className="rounded-full">
+            <IoChatbox className="text-accentV size-6" />
+          </Button>
         </div>
-        <div></div>
+        <div className="size-fit rounded-full overflow-hidden cursor-pointer">
+          <Image
+            src={profile}
+            alt="profile"
+            width={40}
+            height={40}
+            className="md:size-16"
+          />
+        </div>
       </div>
     </nav>
   );
